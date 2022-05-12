f97728d6462e33d837b9f252c529bbdcee6dba60
BrokenBlade4321 <4over3piroga@gmail.com>
2022-05-12 13:46:01 +0300

    verse4

--- main.py
+++ main.py
@@ -2,40 +2,54 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.layers import SimpleRNN, Dense
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.optimizers import RMSprop, Adam
 
+import pandas as pd
 import numpy as np
-
-from random import gauss, randint
-from math import sin
 
 import matplotlib.pyplot as plt
 
+from math import fabs
 
-def random_terrain_generation(lenght: int) -> list:
+def replace_data(path_to_file: str) -> None:
     """
-    функция генерирует случайный рельеф длиной lenght
-    :param lenght: длина рельефа
-    :return: случайный рельеф
+
+    :param path_to_file: путь к файлу
+    :return:
     """
-    value = 0
-    a = randint(500, 1000)
-    random_walk = []
-    for i in range(lenght):
-        value += gauss(a * sin(24 * i / a), a/2)
-        random_walk.append(value)
-    return random_walk
+    with open(path_to_file, "r") as doc:
+        text = doc.read()  # считываем текст
+        if not text.startswith('"'):  # если не начинается с ", то файл еще не изменен
+            rows = text.split("\n")  # делим по строчкам
+            output_text = ''  # текст, который хотим соханить
+            for row in rows:  # проходим по строчкам
+                if row:  # если строчка не пустая
+                    new_row = row.replace(',', '.').split(";")  # меняем запятые на точки,делим текст по ;
+                    output_text += '"' + '","'.join(new_row) + '"\n'  # добавлям new_row к нашему тексте
+        else:
+            output_text = text  # если файл уже был изменен, то это же и сохраним
+    with open(path_to_file, "w") as doc:
+        doc.write(output_text)  # сохраняем измененные данные
 
 
-mountains = [random_terrain_generation(500) for _ in range(50)]
+file_names = [f"profile ({i})" for i in range(1, 16 + 1)]
+
+for file_name in file_names:
+    replace_data(f"{file_name}.csv")
+
+mountains = []
+for file_name in file_names:
+    df = pd.read_csv(f"{file_name}.csv")
+    mountains.append(list(df.loc[:, 'Высота']))
+
 INPUT_DOTS = 7
 X = []
 Y = []
 for elem in mountains:
     X += [elem[i:i + INPUT_DOTS] for i in range(0, len(elem) - INPUT_DOTS - 1, INPUT_DOTS)]
     Y += [[elem[i]] for i in range(INPUT_DOTS + 1, len(elem), INPUT_DOTS)]
-y = np.array(Y)
-x = np.array(X)
+    y = np.array(Y)
+    x = np.array(X)
 
 # линейная регрессия
 # создаем тестовые и тренеровочные данные
@@ -44,16 +58,19 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 model = LinearRegression()
 # обучаем модель
 model.fit(x_train, y_train)
-# оценка модели
-print(model.score(x_test, y_test))
 
-test_mountains = random_terrain_generation(500)
+test_mountains = list(pd.read_csv(r"profile.csv").loc[:, 'Высота'])
 # прогнозируем на тестовых данных
 predict_mountains = model.predict([test_mountains[i:i + INPUT_DOTS] for i in range(len(test_mountains) - INPUT_DOTS)])
 
-# вывод прогноза и настоящих данных
-plt.plot(predict_mountains[:100])
-plt.plot(test_mountains[INPUT_DOTS:INPUT_DOTS + 100])
+model1_fabs = [fabs(predict_mountains[i] - test_mountains[INPUT_DOTS + i]) for i in range(len(predict_mountains))]
+model2_fabs = [fabs(test_mountains[i] - test_mountains[i + 1]) for i in range(len(test_mountains) - 1)]
+model1_mean_err = sum(model1_fabs) / len(predict_mountains)
+model2_mean_err = sum(model2_fabs) / (len(test_mountains) - 1)
+
+print(model1_mean_err / model2_mean_err)
+plt.plot(predict_mountains[:], c="blue")
+plt.plot(test_mountains[INPUT_DOTS:], c="orange")
 
 plt.grid(True)
 plt.show()
@@ -62,27 +79,30 @@ plt.show()
 # создание нейронки с 2 слоями, loss = средний модуль ошибки,
 rnn_model = Sequential()
 rnn_model.add(SimpleRNN(INPUT_DOTS, activation="relu"))
-rnn_model.add(Dense(INPUT_DOTS*2,activation="relu"))
+rnn_model.add(Dense(INPUT_DOTS * 2, activation="relu"))
 rnn_model.add(Dense(1))
-rnn_model.compile(optimizer=RMSprop(), loss='mae')
+rnn_model.compile(optimizer=Adam(0.0001), loss='mae')
 
 # обучаем нейронку
-rnn_model.fit(np.array(x).reshape(len(x), INPUT_DOTS, 1), np.array(y), epochs=60, steps_per_epoch=300, validation_split=0.2)
+rnn_model.fit(np.array(x).reshape(len(x), INPUT_DOTS, 1), np.array(y), epochs=100, steps_per_epoch=200,
+              validation_split=0.2)
 
-while True:
-    # создаем тестовые данные
-    test_mountains = random_terrain_generation(500)
-    test_mountains_array = np.array([test_mountains[i:i + INPUT_DOTS] for i in range(len(test_mountains) - INPUT_DOTS)])
-    # прогнозируем на тестовых данных
-    predict_mountains = rnn_model.predict(test_mountains_array.reshape(len(test_mountains_array), INPUT_DOTS, 1))
-    # вывод прогноза и настоящих данных
-    plt.plot(test_mountains[INPUT_DOTS:INPUT_DOTS + 100])
-    plt.plot(predict_mountains[:100])
+# создаем тестовые данные
+test_mountains = list(pd.read_csv(r"profile.csv").loc[:, 'Высота'])
+test_mountains_array = np.array([test_mountains[i:i + INPUT_DOTS] for i in range(len(test_mountains) - INPUT_DOTS)])
+# прогнозируем на тестовых данных
+predict_mountains = rnn_model.predict(test_mountains_array.reshape(len(test_mountains_array), INPUT_DOTS, 1))
 
-    plt.grid(True)
-    plt.show()
-    plt.plot(test_mountains[INPUT_DOTS:])
-    plt.plot(predict_mountains)
+rnn_fabs = [fabs(predict_mountains[i] - test_mountains[INPUT_DOTS + i]) for i in range(len(predict_mountains))]
+rnn_mean_err = sum(rnn_fabs) / len(predict_mountains)
 
-    plt.grid(True)
-    plt.show()
+print(rnn_mean_err / model2_mean_err)
+
+plt.grid(True)
+plt.show()
+plt.plot(predict_mountains, c="blue")
+plt.plot(test_mountains[INPUT_DOTS:], c="orange")
+
+plt.grid(True)
+plt.show()
+
